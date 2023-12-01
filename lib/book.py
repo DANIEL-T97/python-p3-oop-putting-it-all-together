015532ec10437a37a5cddca4c25ef96a9a79adaf
Daniel <daniel.mwangi@student.moringaschool.com>
2023-12-01 21:25:41 +0300

    commit putting it all together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,15 @@
-#!/usr/bin/env python3
-
 class Book:
     pass
-    
-        
+    def _init_(self, title, page_count:int):
+        assert page_count , "page_count must be an integer"
+
+        self.title = title
+        self.page_count = page_count
+    def turn_page(self):
+        return "Flipping the page...wow,you read fast!"
+
+my_book= Book("fathers of nations", 20)
+
+print(my_book.title)
+print(my_book.page_count)
+print(my_book.turn_page())
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
-
 class Shoe:
     pass
+    def _init_(self, brand, size: int):
+        self.brand = brand
+        assert isinstance(size, int), "Size must be an integer"
+        self.size = size
+        self.condition = 'New'  
+
+    def repair(self):
+        self.condition = 'Repaired'
+        self.cobble = True
+
+
+try:
+    my_shoe = Shoe('adidas', '20') 
+except AssertionError as e:
+    print(e)
+
+my_shoe = Shoe('nike', 10)
+print(my_shoe.condition)  
+
+my_shoe.repair()
+print(my_shoe.condition)  
+print(my_shoe.cobble)  
